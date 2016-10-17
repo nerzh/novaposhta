a1bb98d9d41bf952a0bd30f2637809ff1114e7d2
woodcrust <roboucrop@gmail.com>
2016-10-17 05:02:10 +0300

    fix and ref base

--- lib/novaposhta/base.rb
+++ lib/novaposhta/base.rb
@@ -2,26 +2,52 @@ module Novaposhta
   class Base
     def self.class_attr_accessor(*names)
       names.each do |name|
-        define_singleton_method("#{name.to_s}=".to_sym){ |attr| eval("@@#{name.to_s} = #{attr}") }
-        define_singleton_method(name.to_sym){ eval("@@#{name.to_s}") }
+        define_singleton_method("#{name.to_s}=".to_sym){ |attr| class_variable_set("@@#{name.to_s}", attr) }
+        define_singleton_method(name.to_sym){ class_variable_get("@@#{name.to_s}") }
       end
     end
 
     class_attr_accessor :url, :api_key, :sender, :format
 
-    def post_request(body)
-      url     = URI( self.class.url.gsub(/\{format\}/, "#{self.class.format.to_s}") )
-      request = Net::HTTP::Post.new(url)
-      if self.class.format.to_sym == :json
-        request.content_type = 'application/json'
-        request.body.to_json
-      else
-        raise "#{self.class.format.to_s.upcase} does not work now"
-      end
-      response = Net::HTTP.start(url.host, url.port, :use_ssl => url.scheme == 'https') do |http|
+    def self.post_request(body)
+      uri      = URI( url.gsub(/\{format\}/, "#{format.to_s}") )
+      request  = Net::HTTP::Post.new(uri)
+      format_request!(request, body)
+      response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
         http.request request
       end
-      response.body
+      format_response(response)
+    rescue
+      nil
+    end
+
+    def self.make_body(model, method, options={})
+      {
+        "modelName":        model,
+        "calledMethod":     method,
+        "methodProperties": options,
+        "apiKey":           api_key
+      }
+    end
+
+    private
+
+    def self.format_request!(request, body)
+      if format.to_sym == :json
+        request.content_type = 'application/json'
+        request.body         = body.to_json
+      elsif format.to_sym == :xml
+        request.content_type = 'text/xml'
+        request.body         = body.to_xml.gsub(/\n/, '').gsub(/>\s+</, '><')
+      end
+    end
+
+    def self.format_response(response)
+      if format.to_sym == :json
+        JSON.parse(response.body)
+      elsif format.to_sym == :xml
+        Nokogiri::XML(response.body)
+      end
     end
   end
 end
